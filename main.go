2ea49f93cd797f78021375e28ce31bdac0e00441
Brian Choy <bycEEE@gmail.com>
2018-10-18 11:34:57 -0400

    Improve comments

--- main.go
+++ main.go
@@ -13,20 +13,23 @@ import (
 )
 
 const (
-	cmdIDExit       = 0
-	cmdIDFileRange  = 1
+	// cmdIDExit is sent from tinfoil when all transfers are finished.
+	cmdIDExit = 0
+	// cmdIDFileRange is sent from tinfoil when there are additional transfers.
+	cmdIDFileRange = 1
+	// cmdTypeResponse is the header type.
 	cmdTypeResponse = 1
 )
 
-// NSPList contains information for the payload to be sent to tinfoil
+// NSPList contains information for the payload to be sent to tinfoil.
 type NSPList struct {
-	// Paths is an array of paths to all NSPs to install
+	// Paths is an array of paths to all NSPs to install.
 	Paths []string
-	// Length is the size of the entire payload
+	// Length is the size of the entire payload.
 	Length int
 }
 
-// buildNSPList populates and returns an NSPList struct built from getNSPListFromDirectory
+// buildNSPList populates and returns an NSPList struct built from getNSPListFromDirectory.
 func buildNSPList(f []string) (l NSPList) {
 	var totalLength int
 	for i, path := range f {
@@ -37,28 +40,28 @@ func buildNSPList(f []string) (l NSPList) {
 	return l
 }
 
-// sendNSPPayload creates a payload out of an NSPList struct and sends it to the switch
+// sendNSPPayload creates a payload out of an NSPList struct and sends it to the switch.
 func sendNSPList(l NSPList, epOut *gousb.OutEndpoint) {
 	epOut.Write([]byte("TUL0")) // Tinfoil USB List 0
 
 	buf := make([]byte, 4)
-	binary.LittleEndian.PutUint32(buf, uint32(l.Length))
+	binary.LittleEndian.PutUint32(buf, uint32(l.Length)) // NSP list length
 	epOut.Write(buf)
 
 	buf = make([]byte, 8)
-	copy(buf, strings.Repeat("\x00", 0x8))
+	copy(buf, strings.Repeat("\x00", 0x8)) // Padding
 	epOut.Write(buf)
 
 	fmt.Printf("Sending NSP list: %v", l)
 
 	for _, path := range l.Paths {
 		buf = make([]byte, len(path))
-		copy(buf, path)
+		copy(buf, path) // File path followed by newline
 		epOut.Write(buf)
 	}
 }
 
-// sendNSPFiles handles sending files to the switch
+// sendNSPFiles handles sending files to the switch.
 func sendNSPFiles(l NSPList, epIn *gousb.InEndpoint, epOut *gousb.OutEndpoint, dataSize uint64) {
 	buf := make([]byte, 32)
 	_, err := epIn.Read(buf)
@@ -69,7 +72,7 @@ func sendNSPFiles(l NSPList, epIn *gousb.InEndpoint, epOut *gousb.OutEndpoint, d
 	rangeSize := binary.LittleEndian.Uint64(fileRangeHeader[:8])
 	rangeOffset := binary.LittleEndian.Uint64(fileRangeHeader[8:16])
 	nspNameLength := binary.LittleEndian.Uint64(fileRangeHeader[16:24])
-	buf = buf[:0] // reset buffer
+	buf = buf[:0] // reset buffer (might be unneeded, left over from resolving previous issues)
 
 	buf = make([]byte, nspNameLength)
 	_, err = epIn.Read(buf)
@@ -77,7 +80,7 @@ func sendNSPFiles(l NSPList, epIn *gousb.InEndpoint, epOut *gousb.OutEndpoint, d
 		log.Fatalf("Reading NSP name failed: %v", err)
 	}
 	nspName := string(buf)
-	buf = buf[:0] // clear buffer
+	buf = buf[:0] // reset buffer (might be unneeded, left over from resolving previous issues)
 	fmt.Printf("Range size: %d, Range offset: %d, Name length: %d, Name: %s\n",
 		rangeSize, rangeOffset, nspNameLength, nspName)
 
@@ -124,7 +127,7 @@ func sendNSPFiles(l NSPList, epIn *gousb.InEndpoint, epOut *gousb.OutEndpoint, d
 	}
 }
 
-// sendFiles wraps around sendNSPFiles and keeps the connection open
+// sendFiles wraps around sendNSPFiles and keeps the connection open.
 func sendNSPFilesPoll(l NSPList, epIn *gousb.InEndpoint, epOut *gousb.OutEndpoint) {
 	for {
 		buf := make([]byte, 32)
@@ -141,7 +144,7 @@ func sendNSPFilesPoll(l NSPList, epIn *gousb.InEndpoint, epOut *gousb.OutEndpoin
 		cmdType := cmdHeader[4:5]
 		cmdID := binary.LittleEndian.Uint32(cmdHeader[8:12])
 		dataSize := binary.LittleEndian.Uint64(cmdHeader[12:20])
-		buf = buf[:0] // reset buffer
+		buf = buf[:0] // reset buffer (might be unneeded, left over from resolving previous issues)
 		fmt.Printf("Cmd type: %d, Command ID: %d, Data size: %d\n", cmdType, cmdID, dataSize)
 		if cmdID == cmdIDExit {
 			fmt.Println("Finished transfer, exiting")
@@ -152,7 +155,7 @@ func sendNSPFilesPoll(l NSPList, epIn *gousb.InEndpoint, epOut *gousb.OutEndpoin
 	}
 }
 
-// getInOutEndpoints retrieves the in and out endpoints
+// getInOutEndpoints retrieves the in and out endpoints.
 func getInOutEndpoints(intf *gousb.Interface) (in *gousb.InEndpoint, out *gousb.OutEndpoint, err error) {
 	for _, desc := range intf.Setting.Endpoints {
 		if desc.Direction == true {
@@ -164,7 +167,7 @@ func getInOutEndpoints(intf *gousb.Interface) (in *gousb.InEndpoint, out *gousb.
 	return in, out, err
 }
 
-// getNSPListFromDirectory list NSPs in a directory and its subdirectories
+// getNSPListFromDirectory list NSPs in a directory and its subdirectories.
 func getNSPListFromDirectory(d string) []string {
 	if _, err := os.Stat(d); os.IsNotExist(err) {
 		log.Fatal("NSP directory does not exist")
@@ -201,18 +204,17 @@ func main() {
 		log.Fatalf("supplied path is not a directory: %s", dir)
 	}
 
-	// Initialize a new Context.
+	// Initialize a new Context for Switch USB device
 	ctx := gousb.NewContext()
 	defer ctx.Close()
 
-	// Iterate through available Devices, finding all that match a known VID/PID.
-	vid, pid := gousb.ID(0x057E), gousb.ID(0x3000)
+	// Find Switch
+	vid, pid := gousb.ID(0x057E), gousb.ID(0x3000) // https://github.com/XorTroll/Tinfoil/blob/21941dd9219149e2cc598e8a967343abffc6f883/source/nx/ipc/usb_comms_new.c#L71
 	devs, err := ctx.OpenDevices(func(desc *gousb.DeviceDesc) bool {
-		// this function is called for every device present.
-		// Returning true means the device should be opened.
 		return desc.Vendor == vid && desc.Product == pid
 	})
-	// All returned devices are now open and will need to be closed.
+
+	// All returned devices are now open and will need to be closed
 	for _, d := range devs {
 		defer d.Close()
 	}
@@ -223,31 +225,31 @@ func main() {
 		log.Fatalf("no devices found matching VID %s and PID %s", vid, pid)
 	}
 
-	// Pick the first device found.
+	// Pick the first device found
 	dev := devs[0]
 
-	// Get active configuration.
+	// Get active configuration
 	activeConfig, err := dev.ActiveConfigNum()
 	if err != nil {
 		log.Fatalf("failed to get active config: %v", err)
 	}
 
-	// Switch the configuration.
+	// Switch the configuration
 	cfg, err := dev.Config(activeConfig)
 	if err != nil {
 		log.Fatalf("%s.Config(%d): %v", dev, activeConfig, err)
 	}
 	defer cfg.Close()
 
-	// In the config claim interface #0 with alt setting #0.
+	// In the config claim interface #0 with alt setting #0
 	intf, done, _ := dev.DefaultInterface()
 	if err != nil {
 		log.Fatalf("%s.Interface(0, 0): %v", cfg, err)
 	}
 	defer done()
 
+	// Interface with tinfoil
 	epIn, epOut, err := getInOutEndpoints(intf)
-	fmt.Println(epIn)
 	NSPList := buildNSPList(getNSPListFromDirectory(dir))
 	sendNSPList(NSPList, epOut)
 	sendNSPFilesPoll(NSPList, epIn, epOut)
